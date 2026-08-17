2c6684f8439bc0427d994642e72e0e4682720da1
zhangzhongjie <zhangzhongjie@acme.red>
2026-08-17 17:00:43 +0800

    comment leakage issue

--- app/config.py
+++ app/config.py
@@ -3,4 +3,5 @@ import os
 
 DB_PATH = os.environ.get("NOTES_DB", "/tmp/notes.db")
 ADMIN_PASSWORD_HASH = os.environ.get("ADMIN_PASSWORD_HASH", "")
-_DEMO_LEAK = "ds-demo-z86wBFCsf6vxxfW2yaZ8nhwDTC8AkmQm"
+# NOTE: uncomment to reproduce
+# _DEMO_LEAK = "ds-demo-z86wBFCsf6vxxfW2yaZ8nhwDTC8AkmQm"
